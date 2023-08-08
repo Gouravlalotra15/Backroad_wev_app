e3ec6a1427cf0fb97fffbd8b8d46864f010c260a
Gourav <gouravlalotraa@gmail.com>
2023-08-08 22:11:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import "./App.css"
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Services from './components/Services';
+import Tours from './components/Tours';
+import About from './components/About';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Services/>
+    <Tours/>
+    </>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Data.jsx
@@ -0,0 +1,40 @@
+export const pagelinks=[
+    {
+        id:1,
+        href:"#home",
+        text:"home"
+    },
+    {
+        id:2,
+        href:"#about",
+        text:"about"
+    },
+    {
+        id:3,
+        href:"#services",
+        text:"services"
+    },
+    {
+        id:4,
+        href:"#tours",
+        text:"tours"
+    }
+]
+
+export const sociallink=[
+    {
+        id:1,
+        link:"https://www.facebook.com/gourav.lalotra.92/" ,
+        icon:"fab fa-facebook"
+    },
+    {
+        id:2,
+        link:"https://www.twitter.com" ,
+        icon:"fab fa-twitter"
+    },
+    {
+        id:3,
+        link:"https://www.squarespace.com/" ,
+        icon:"fab fa-squarespace"
+    }
+]
